e722d752c961f5793c0aaf2ea3e0916bcf5a7f63
Paul Reichelt <paul.reichelt@outlook.com>
2017-12-05 23:25:30 +0100

    Hello World

---
+++ index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import * as ReactDOM from 'react-dom'
+
+const App = () => {
+  return (
+    <h1>Hello World!</h1>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
